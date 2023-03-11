fe5823016d5d31fb1b6bc8202b78e607a1c51a1c
Thao.Tran <thaotran.k39ueh.qb@gmail.com>
2023-03-11 21:08:56 +0700

    Configie service and api url

--- AspNetCoreApi/Program.cs
+++ AspNetCoreApi/Program.cs
@@ -1,6 +1,11 @@
+using AspNetCoreApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntergartion();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
