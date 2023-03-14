82ae0a043060e616f8d56e6db79c855ce74d5dc3
Thao.Tran <thaotran.k39ueh.qb@gmail.com>
2023-03-15 00:09:48 +0700

    Add owner controller

---
+++ AspNetCoreApi/Controllers/OwnerController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreApi.Controllers
+{
+    [Route("api/owner")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+        private IMapper _mapper;
+
+        public OwnerController(ILoggerManager logger, IRepositoryWrapper repository, IMapper mapper)
+        {
+            _logger = logger;
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOwners()
+        {
+            try
+            {
+                var owners = _repository.Owner.GetAllOwners();
+                _logger.LogInfo($"Return {owners.Count()} owners from database. ");
+                var ownerResult = _mapper.Map<IEnumerable<OwnerDTO>>(owners);
+                return Ok(ownerResult);
+            }catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllOwners action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+    }
+}
